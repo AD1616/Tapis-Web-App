706901f0f4748c6557a2eff7994153566f5d4eca
Sahil Samar <sahilsamar031@gmail.com>
2022-10-28 01:54:46 -0700

    home template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 import pandas as pd
@@ -9,10 +10,13 @@ import neo4jupyter
 
 @app.route('/')
 def home():
-    host = "bolt+s://catalog.pods.icicle.tapis.io:443"
-    username = "catalog"
-    password = "3hpnfBio0OJ5sHAF5ZzBUDWz0db90i"
-    graph = Graph(host, auth=(username, password))
-    query = "MATCH(n:COMPONENT) RETURN n LIMIT 5"
-    print(graph.run(query).to_data_frame())
-    return ''
+    # host = "bolt+s://catalog.pods.icicle.tapis.io:443"
+    # username = "catalog"
+    # password = "3hpnfBio0OJ5sHAF5ZzBUDWz0db90i"
+    # graph = Graph(host, auth=(username, password))
+    # query = "MATCH(n:COMPONENT) RETURN n LIMIT 5"
+    # print(graph.run(query).to_data_frame())
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
